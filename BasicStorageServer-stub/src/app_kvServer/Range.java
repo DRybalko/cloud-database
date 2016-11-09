93a744b6ebd5a26a85d863df95c0c1eb8101a5b7
GerganaKr <gergana_kratuncheva@abv.bg>
2016-11-09 21:29:02 +0100

    Range

--- BasicStorageServer-stub/src/app_kvServer/Range.java
+++ BasicStorageServer-stub/src/app_kvServer/Range.java
@@ -2,4 +2,26 @@ package app_kvServer;
 
 public class Range {
 
+	private String from;
+	private String to;
+	
+	
+	public Range (String from, String to){
+		this.from = from;
+		this.to = to;
+	}
+	
+	public void setFrom(String from){
+		this.from = from;
+	}
+	public String getFrom(){
+		return from;
+	}
+	
+	public void setTo(String to){
+		this.to = to;
+	}
+	public String getTo(){
+		return to;
+	}
 }
