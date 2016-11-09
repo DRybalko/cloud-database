5a0d9fb3e9bbc36158416c41b2287758c1c22596
GerganaKr <gergana_kratuncheva@abv.bg>
2016-11-09 23:42:20 +0100

    Range

--- BasicStorageServer-stub/src/app_kvServer/Range.java
+++ BasicStorageServer-stub/src/app_kvServer/Range.java
@@ -4,11 +4,39 @@ public class Range {
 
 	private String from;
 	private String to;
+	private static final byte SEPARATOR = 45;
 	
 	
 	public Range (String from, String to){
 		this.from = from;
 		this.to = to;
+	}
+	
+
+	
+	public Range(byte[] bytes){
+		int i=0;		        
+        while(bytes[i] != SEPARATOR){
+        	i++;
+        }       
+        byte [] from = new byte[i];
+        byte [] to = new byte[bytes.length - i-1];
+               
+        for(int j = 0; j<= i-1; j++){
+        	from[j] = bytes[j];
+        }
+        
+        this.from = new String(from);
+		
+		for(int j = 0; j<bytes.length - i-1; j++){
+			to[j] = bytes[j+i+1];
+		}
+        this.to = new String(to);	
+	}
+	
+
+	public String getLower_limit() {
+		return from;
 	}
 	
 	public void setFrom(String from){
@@ -24,4 +52,5 @@ public class Range {
 	public String getTo(){
 		return to;
 	}
+
 }
